2ffe67d133e91b3a1927a1d31bb63ff423596026
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-01 23:53:09 -0500

    Link to spaceship project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # csc151
 Projects from class CSC-151. Functional Problem Solving
+<a href="spaceship.html">Rainbow Spaceship Project</a>
---
+++ spaceship.scm
@@ -0,0 +1,39 @@
+(import image)
+
+; spaceship.scm
+;
+;
+; CSC 151 02 (23fa)
+; Mini Project 1, Part 1
+; Author: Khanh Do
+; Date: 2022-09-04
+; Acknowledgements: Following the instructions of the 
+; mini-project 1 on the CSC-151 website.
+
+; (...code below here...)  
+;
+(define stripe
+   (lambda (color)
+     (rectangle 100 25 "solid" color)))
+
+(define stripe-1 (stripe "red"))
+(define stripe-2 (above stripe-1 (stripe "orange")))
+(define stripe-3 (above stripe-2 (stripe "yellow")))
+(define stripe-4 (above stripe-3 (stripe "green")))
+(define stripe-5 (above stripe-4 (stripe "blue")))
+(define stripe-6 (above stripe-5 (stripe "violet")))
+
+(define rainbow-spaceship
+  (beside stripe-1 
+          stripe-2
+          stripe-3
+          stripe-4
+          stripe-5
+          stripe-6
+          stripe-5
+          stripe-4
+          stripe-3
+          stripe-2
+          stripe-1))
+
+(rainbow-spaceship)
