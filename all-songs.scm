f228c9e8b252035224133378f4abafad9eb78609
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-07 14:48:03 -0500

    import lab, test, add docstring for problem 1

--- all-songs.scm
+++ all-songs.scm
@@ -4,3 +4,15 @@
 ;; 2023-10-13
 ;; ACKNOWLEDGEMENTS:
 ;;   Following instructions on Mini-Project 4 page
+
+(import lab)
+(import test)
+
+(problem "Part 1: Cartesian Product")
+
+"all-list2"
+
+;;; (all-list2 v) -> list?
+;;;   v: any?
+;;; Takes a value v and a list of values and creates a list of all possible lists of size 2
+;;; where the first element is v and the second element is a value from lst.
