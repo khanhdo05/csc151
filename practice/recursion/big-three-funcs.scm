6d52fb1987217d284b8a8d7b53e8d467eb924fbc
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-11 23:37:28 -0500

    documentation

---
+++ practice/recursion/big-three-funcs.scm
@@ -0,0 +1,16 @@
+;; CSC 151 (Fall 2023)
+;; Lab: Implementing the Big Three
+;; Authors: Khanh Do, Paul Lim
+;; Date: Oct 11, 2023
+;; Acknowledgements: N/A
+
+;; Whew! We've spent a solid week and a half drilling recursive design 
+;; techniques over lists and the natural numbers. But this only the beginning!
+;; We will deepen our knowledge of recursive design throughout the remainder
+;; course as we explore more intricate and complex problems. As a starting
+;; point in this conversation, we'll look at the patterns of recursion that
+;; we have developed so far and how they relate to the "big three" operations
+;; over lists we encountered earlier in the course.
+(import lab)
+(import test)
+;; -------------------------
