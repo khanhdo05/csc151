a84300248d4627a005e8645b005f6a0407f4511d
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-22 14:56:52 -0500

    define dict inventory is a list of (pair key value)

--- practice/recursion/assoc.scm
+++ practice/recursion/assoc.scm
@@ -6,3 +6,5 @@
 ; of our inventory. Write a function (assoc-update-inc-by d k n) that returns
 ; a new dictionary that is d but updates key-value pair (pair k v) of d to be
 ; (k (+ v n)). That is, we add n to the value associated with k in d.
+
+(define inventory (list (pair "apples" 5) (pair "bananas" 2) (pair "oranges" 8)))
