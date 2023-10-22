1134a4f334442a6d777354274f68b0be9d3c2369
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-22 14:57:23 -0500

    docstrings for assoc-update-inc-by

--- practice/recursion/assoc.scm
+++ practice/recursion/assoc.scm
@@ -8,3 +8,9 @@
 ; (k (+ v n)). That is, we add n to the value associated with k in d.
 
 (define inventory (list (pair "apples" 5) (pair "bananas" 2) (pair "oranges" 8)))
+
+;;; (assoc-update-inc-by d k n) -> list?
+;;;   d: list?
+;;;   k: any
+;;;   n: any
+;;; Returns dictionary d but updates key-value pair (pair k v) of d to be (k (+ v n))
