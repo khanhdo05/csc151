d61d5e5c9c0563f7d0bf9ed7b325ed18ae5e5ce3
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-23 15:32:15 -0500

    lab 1st prob

--- practice/recursion/assoc.scm
+++ practice/recursion/assoc.scm
@@ -37,3 +37,55 @@
            (lambda () (assoc-update-inc-by inventory "mango" 3)))
 
 (test-exn "wrong input for d" (lambda () (assoc-update-inc-by 4 "hello" "world")))
+
+;; -------------------------------
+"Problem 1: Guilty by association"
+;; -------------------------------
+
+;; (Partner A drives!)
+;;
+;; Recall the set up for an association list:
+;;
+;; + An association list is a set of pairs.
+;; + Each pair contains a key and a value.
+;; + The key is used to "look up" its associated value in the list.
+;;
+;; For this first problem, we'll use an association list to maintain a roster
+;; of students---mad scientists---and their attendance in class at mad
+;; scientist univeristy. Complete the definition roster below as an
+;; association list that makes the test cases below pass.
+
+(define example-roster
+  (list (pair "Prof. Froth" 5)
+        (pair "Dr. Spark" 5)
+        (pair "Prof. Afterthought" 2)
+        (pair "Dr. Agon" 3)
+        (pair "Dr. Putrid" 5)))
+
+; Mad scientist names generated from Fantasy Name Generator:
+; https://www.fantasynamegenerators.com/mad-scientist-names.php
+
+(test-case "Prof. Froth is in the class"
+  equal? #t (lambda () (assoc-key? "Prof. Froth" example-roster)))
+(test-case "Prof. Pride is not in the class"
+  equal? #f (lambda () (assoc-key? "Prof. Pride" example-roster)))
+
+(test-case "Dr. Spark's attendance"
+  = 5 (lambda () (assoc-ref "Dr. Spark" example-roster)))
+(test-case "Prof. Froth's attendance"
+  = 5 (lambda () (assoc-ref "Prof. Froth" example-roster)))
+(test-case "Prof. Afterthought's attendance"
+  = 2 (lambda () (assoc-ref "Prof. Afterthought" example-roster)))
+(test-case "Dr. Agon's attendance"
+  = 3 (lambda () (assoc-ref "Dr. Agon" example-roster)))
+(test-case "Dr. Putrid's attendence"
+  = 5 (lambda () (assoc-ref "Dr. Putrid" example-roster)))
+
+;; The test cases demonstrate the use of two of the three association list
+;; functions provided in the standard library: assoc-key? and assoc-ref.
+;; Frequently, we'll use assoc-key? to first check if a key is in the
+;; association list. If so, we know that we can then use assoc-ref to
+;; safely pull out its corresponding value.
+;;
+;; Use these functions to implement the following function and give
+;; appropriate test cases for the function using example-roster above.
