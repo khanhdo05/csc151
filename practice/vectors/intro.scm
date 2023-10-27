f5014163bcfca164484d68598f0219d85103c600
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-10-27 00:00:34 -0500

    Write headings

---
+++ practice/vectors/intro.scm
@@ -0,0 +1,8 @@
+;; CSC 151 (Fall 2023)
+;; Lab: Vectors (vectors.scm)
+;; Authors: Khanh Do, Paul Lim
+;; Date: 10.25.2023
+;; Acknowledgements: N/A
+
+;; In today's lab, we'll explore how we can create and manipulate vectors in
+;; Scheme. For each problem, alternative driver/navigator with your partner.
