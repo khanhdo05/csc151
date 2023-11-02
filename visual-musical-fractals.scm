e0540b24d2491ed8a10decdc04589d9d91a42b1b
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-11-01 23:03:14 -0500

    docstrings for cantor-set

--- visual-musical-fractals.scm
+++ visual-musical-fractals.scm
@@ -9,3 +9,15 @@
 (import test)
 (import music)
 (import image)
+
+(part "Part 1: Visual Fractals")
+
+(problem "Cantor Set")
+;;; (cantor-set width height fill color n) -> drawing?
+;;;   width: integer?, non-negative
+;;;   height: integer?, non-negative
+;;;   fill: string?, either "solid" or "outline"
+;;;   color: string?
+;;;   n: integer?, non-negative
+;;; Draws a Cantor Set of depth n with the given visual properties. width 
+;;; and height are the dimensions of a single bar.
