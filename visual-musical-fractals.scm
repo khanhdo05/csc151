1385ac4631f474e53b8c6ca325355011c3190299
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-11-03 01:02:16 -0500

    finish project

--- visual-musical-fractals.scm
+++ visual-musical-fractals.scm
@@ -38,7 +38,6 @@
              (cantor-set (/ width 3) height fill color (- n 1))
              (rectangle (/ width 3) height "solid" "transparent")
              (cantor-set (/ width 3) height fill color (- n 1))))]))))
-
 "n = 1"
 (cantor-set 100 10 "solid" "green" 1)
 "n = 2"
@@ -49,7 +48,7 @@
 (cantor-set 100 10 "solid" "black" 10)
 
 ;_______________________________________________________________
-(problem "Serpiński Carpet")
+(problem "Sierpiński Carpet")
 
 ;;; (serpinski-carpet length fill color n) -> drawing?
 ;;;   length: integer?, non-negative
@@ -81,9 +80,28 @@
 (serpinski-carpet 100 "solid" "black" 3)
 "n = 5"
 (serpinski-carpet 300 "solid" "purple" 5)
+"n = 6"
+(serpinski-carpet 400 "solid" "pink" 6)
 
 ;_______________________________________________________________
 (problem "My Fractal")
+
+;;; (diamond length color) -> drawing?
+;;;   length: integer?, non-negative
+;;;   color: string?
+;;; Draws a rotated 45 degrees square to make a diamond. Adapted from 
+;;; basic-datatypes.scm (mini-project 2)
+(define diamond
+  (lambda (length color) 
+    (path length                           ; horizontal image size
+          length                           ; vertical image size
+          (list (pair (/ length 2) 0)      ; top point
+                (pair length (/ length 2)) ; far right point
+                (pair (/ length 2) length) ; bottom point
+                (pair 0 (/ length 2))      ; far left point
+                (pair (/ length 2) 0))     ; top point (need to return)
+          "outline"                        ; fill style
+          color)))                         ; color
 
 ;;; (my-fractal-helper length color n) -> drawing?
 ;;;   length: integer?, non-negative
@@ -99,7 +117,9 @@
                 [space (square (/ length 3) "solid" "white")]
                 [top-bot-row (beside space make-diamond space)]
                 [mid-row (beside make-diamond make-diamond make-diamond)])
-           (above top-bot-row mid-row top-bot-row))])))
+           (above top-bot-row 
+                  mid-row 
+                  top-bot-row))])))
 
 ;;; (my-fractal length color1 fill color2 n) -> drawing?
 ;;;   length: integer?, non-negative
@@ -198,26 +218,30 @@
 ;;; Creates a musical fractal composition.
 (define my-musical-fractal
   (lambda (lo hi d n)
-    (let ([d3 (div d 3)]
+    (let* ([d3 (div d 3)]
           [d4 (div d 4)]
-          [mid (/ (+ lo hi) 2)])
+          [mid (/ (+ lo hi) 2)]
+          ; perfect fifth
+          [1.5lo (* lo 1.5)]
+          [1.5mid (* mid 1.5)]
+          [1.5hi (* hi 1.5)])
       (match n
         [0 (seq 
-             (par (note-freq lo d4) (note-freq (* lo 1.5) d4))
-             (par (note-freq mid d4) (note-freq (* mid 1.5) d4))
-             (par (note-freq hi d4) (note-freq (* hi 1.5) d4))
-             (par (note-freq mid d4) (note-freq (* mid 1.5) d4)))]
+             (par (note-freq lo d4) (note-freq 1.5lo d4))
+             (par (note-freq mid d4) (note-freq 1.5mid d4))
+             (par (note-freq hi d4) (note-freq 1.5hi d4))
+             (par (note-freq mid d4) (note-freq 1.5mid d4)))]
         [_ (seq
              (par 
                (my-musical-fractal lo mid d3 (- n 1)) 
-               (my-musical-fractal (* lo 1.5) (* mid 1.5) d3 (- n 1)))
+               (my-musical-fractal 1.5lo 1.5mid d3 (- n 1)))
              (par 
                (my-musical-fractal mid hi d3 (- n 1)) 
-               (my-musical-fractal (* mid 1.5) (* hi 1.5) d3 (- n 1)))
+               (my-musical-fractal 1.5mid 1.5hi d3 (- n 1)))
              (seq 
-               (par (note-freq mid d3) (note-freq (* mid 1.5) d3))
-               (par (note-freq hi d3) (note-freq (* hi 1.5) d3))
-               (par (note-freq lo d3) (note-freq (* lo 1.5) d3))))]))))
+               (par (note-freq mid d3) (note-freq 1.5mid d3))
+               (par (note-freq hi d3) (note-freq 1.5hi d3))
+               (par (note-freq lo d3) (note-freq 1.5lo d3))))]))))
 
 ; Example
 (my-musical-fractal 100 500 qn 2)
