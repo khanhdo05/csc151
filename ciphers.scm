0fbc2ab2edaeec784a8b9573dc88bb078ca3489a
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-11-07 16:29:56 -0600

    shuffle! func

--- ciphers.scm
+++ ciphers.scm
@@ -7,3 +7,41 @@
 
 (import lab)
 (import test)
+
+;---------------------------------------------
+(part "Part 1. Creating Substitution Ciphers")
+;---------------------------------------------
+
+(define example-alphabet (string->list "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "))
+
+;;; (swap! vec i j) -> void
+;;;   vec : vector?
+;;;   i : integer?
+;;;   j : integer?
+;;; Swaps the ith and jth entries in the provided vector vec.
+(define swap!
+  (lambda (vec i j)
+    (let ([temp (vector-ref vec i)])
+      (begin
+        (vector-set! vec i (vector-ref vec j))
+        (vector-set! vec j temp)))))
+
+;;; (shuffle-helper! vec n) -> void
+;;;   vec : vector?
+;;;   n : integer?
+;;; Recursively selects the next random element and swaps it into location n.
+(define shuffle-helper!
+  (lambda (vec n)
+    (if (zero? n)
+      void
+      (begin
+        (swap! vec (random (+ n 1)) n)
+        (shuffle-helper! vec (- n 1))
+        void))))
+
+;;; (shuffle! vec) -> void
+;;;   vec : vector?
+;;; Randomly shuffles the elements of the input vector.
+(define shuffle!
+  (lambda (vec)
+    (shuffle-helper! vec ( - (vector-length vec) 1))))
