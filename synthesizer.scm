33192bfdf5894f1550097e5896d086100ad136cb
Khanh <112164825+khanhdo05@users.noreply.github.com>
2023-11-21 10:50:17 -0600

    Heading

---
+++ synthesizer.scm
@@ -0,0 +1,8 @@
+; synthesizer.scm
+
+;; CSC-151-02 (Fall 23)
+;; Mini-Project 7: A Simple Synthesizer
+;; Khanh Do
+;; 2023-11-19
+;; ACKNOWLEDGEMENTS: every functions that help define function synthesize-square-note
+;; are adpated from asdr lab.
